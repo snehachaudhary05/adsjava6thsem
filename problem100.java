d296b48fc4aaf30b5d12f2cfd592d2743dba9742
Sneha Chaudhary <160459629+snehachaudhary05@users.noreply.github.com>
2025-03-06 12:04:38 +0530

    Add files via upload

---
+++ problem100.java
@@ -0,0 +1,13 @@
+import javax.swing.tree.TreeNode;
+
+public class problem100 {
+     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null || q==null || p.val!=q.val){
+            return false;
+        }
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
