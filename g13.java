813667d1f11e149c85025de6f83220b08ec4068a
Sneha Chaudhary <160459629+snehachaudhary05@users.noreply.github.com>
2025-03-18 11:42:32 +0530

    Add files via upload

---
+++ g13.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class g13 {
+    // Function to find length of longest substring without repeating characters
+    public static int lenLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println("Length of longest substring: " + lenLongestSubstring(s));
+    }
+}
